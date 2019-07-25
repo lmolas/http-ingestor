7c4c3922044345cc22f548b8aa7e3a748d0c6a33
lmolas <lionel.molas@continental-corporation.com>
2019-07-25 11:54:19 +0200

    Remove some fmt.Printf

--- main.go
+++ main.go
@@ -2,11 +2,10 @@
 package main
 
 import (
-	_ "expvar"
 	"flag"
-	"io/ioutil"
 	"log"
 	"net/http"
+
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
@@ -46,11 +45,11 @@ func (w Worker) start() {
 			select {
 			case job := <-w.jobQueue:
 				// Dispatcher has added a job to my jobQueue.
-				log.Printf("worker%d: started %s, blocking for %f seconds\n", w.id, job.Name, job.Delay.Seconds())
-				log.Printf("worker%d: started %s, blocking for %f seconds\n", w.id, job.Name, job.Delay.Seconds())
+				// log.Printf("worker%d: started %s, blocking for %f seconds\n", w.id, job.Name, job.Delay.Seconds())
+				// log.Printf("worker%d: started %s, blocking for %f seconds\n", w.id, job.Name, job.Delay.Seconds())
 				time.Sleep(job.Delay)
-				log.Printf("worker%d: completed %s!\n", w.id, job.Name)
-				log.Printf("worker%d: completed %s!\n", w.id, job.Name)
+				// log.Printf("worker%d: completed %s!\n", w.id, job.Name)
+				// log.Printf("worker%d: completed %s!\n", w.id, job.Name)
 			case <-w.quitChan:
 				// We have been asked to stop.
 				log.Printf("worker%d stopping\n", w.id)
@@ -101,9 +100,9 @@ func (d *Dispatcher) dispatch() {
 		select {
 		case job := <-d.jobQueue:
 			go func() {
-				log.Printf("fetching workerJobQueue for: %s\n", job.Name)
+				// log.Printf("fetching workerJobQueue for: %s\n", job.Name)
 				workerJobQueue := <-d.workerPool
-				log.Printf("adding %s to workerJobQueue\n", job.Name)
+				// log.Printf("adding %s to workerJobQueue\n", job.Name)
 				workerJobQueue <- job
 			}()
 		}
@@ -125,11 +124,13 @@ func requestHandler(w http.ResponseWriter, r *http.Request, jobQueue chan Job) {
 		return
 	}
 
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("body = %s\n", string(body))
+	// body, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// log.Printf("body = %s\n", string(body))
+
+	// defer r.Body.Close()
 
 	// Parse the delay.
 	// delay, err := time.ParseDuration(r.FormValue("delay"))
@@ -173,7 +174,7 @@ type Service struct {
 
 func main() {
 	var (
-		maxWorkers   = flag.Int("max_workers", 5, "The number of workers to start")
+		maxWorkers   = flag.Int("max_workers", 20, "The number of workers to start")
 		maxQueueSize = flag.Int("max_queue_size", 100, "The size of job queue")
 		port         = flag.String("port", "8080", "The server port")
 	)
